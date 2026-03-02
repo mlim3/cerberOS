e3a576d3a70671c12e1daf7019562a28f2072f1c
David Shubov <david.shubov@gmail.com>
2026-03-01 21:09:03 -0800

    add: runner

---
+++ vault/engine/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	engine "github.com/mlim3/cerberOS/vault/engine/vm"
+)
+
+func main() {
+	cfg := engine.DefaultVMConfig()
+
+	if v := os.Getenv("KERNEL_IMAGE_PATH"); v != "" {
+		cfg.KernelImagePath = v
+	}
+	if v := os.Getenv("INITRD_PATH"); v != "" {
+		cfg.InitrdPath = v
+	}
+	if v := os.Getenv("QEMU_ACCEL"); v != "" {
+		cfg.Accel = v
+	}
+
+	vm := engine.NewVM(cfg)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	fmt.Println("starting vm...")
+	if err := vm.Start(ctx); err != nil {
+		log.Fatalf("failed to start vm: %v", err)
+	}
+
+	<-ctx.Done()
+	fmt.Println("shutting down...")
+	if err := vm.Stop(); err != nil {
+		log.Printf("error stopping vm: %v", err)
+	}
+}
